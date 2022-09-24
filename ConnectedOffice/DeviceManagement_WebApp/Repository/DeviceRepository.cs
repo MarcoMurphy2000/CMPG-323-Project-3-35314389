a50be5eca14660d19699de05b14b662e6ebc79b0
Marco van der Merwe <Marcovandermerwe2000@gmail.com>
2022-09-24 18:31:31 +0200

    First commit with working project 3

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/DeviceRepository.cs
@@ -0,0 +1,20 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class DeviceRepository : GenericRepository<Device>, IDeviceRepository
+    {
+        public DeviceRepository(ConnectedOfficeContext context) : base(context)
+        {
+        }
+
+        // Get Most Recent Device: Retrieve the category that was created last.
+        public Device GetMostRecentDevice()
+        {
+            return _context.Device.OrderByDescending(device => device.DateCreated).FirstOrDefault();
+        }
+    }
+
+}
---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/ZoneRepository.cs
@@ -0,0 +1,20 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class ZoneRepository : GenericRepository<Zone>, IZoneRepository
+    {
+        public ZoneRepository(ConnectedOfficeContext context) : base(context)
+        {
+        }
+
+        // Get Most Recent Zone: Retrieve the zone that was created last.
+        public Zone GetMostRecentZone()
+        {
+            return _context.Zone.OrderByDescending(zone => zone.DateCreated).FirstOrDefault();
+        }
+    }
+
+}
